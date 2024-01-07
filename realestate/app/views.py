b84006709fbe15c1a9d32194e309a020f79af238
narimanvt <narimanvatankhah81@gmail.com>
2024-01-07 11:26:15 +0330

    wish i don't crash it

--- realestate/app/views.py
+++ realestate/app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 # Create your views here.
 
 def app(request):
-    return HttpResponse("Hello World!")
+    template = loader.get_template('first-page.html')
+    return HttpResponse(template.render())
