afe3dc0187e26ce86e9739568930021b72224b0c
Mina.Modirizadeh <Mina.modirizadeh@gmail.com>
2024-01-10 19:51:22 +0330

    modified urls

--- realestate/realestate/urls.py
+++ realestate/realestate/urls.py
@@ -18,5 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('Estates_and_Locations.urls', namespace='Estates_and_Locations'))
+    path('', include('Estates_and_Locations.urls', namespace='Estates_and_Locations')),
+    path('', include('Clients_and_Contacts.urls', namespace='Clients_and_Contacts')),
+    path('', include('Contracts_and_Transactions', namespace='Contracts_and_Transactions')),
 ]
