fc64155ac3a221c2bf977f7d0259cd742a2a7c2b
Mahdye-Asadi <mahdye.asaadi@gmail.com>
2024-01-10 20:51:14 +0330

    import in admin

--- realestate/Estates_and_Locations/admin.py
+++ realestate/Estates_and_Locations/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Estate, City, Country, Estate_type, Estate_status, In_charge
 
-# Register your models here.
+admin.site.register(Estate)
+admin.site.register(City)
+admin.site.register(Country)
+admin.site.register(Estate_status)
+admin.site.register(Estate_type)
+admin.site.register(In_charge)
