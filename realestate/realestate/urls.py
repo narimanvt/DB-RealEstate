55fdb411c7243a9712344aa351a86e38b3bb699d
narimanvt <narimanvatankhah81@gmail.com>
2024-01-10 20:30:00 +0330

    fixing bugs

--- realestate/realestate/urls.py
+++ realestate/realestate/urls.py
@@ -20,5 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('Estates_and_Locations.urls', namespace='Estates_and_Locations')),
     path('', include('Clients_and_Contacts.urls', namespace='Clients_and_Contacts')),
-    path('', include('Contracts_and_Transactions', namespace='Contracts_and_Transactions')),
+    path('', include('Contracts_and_Transactions.urls', namespace='Contracts_and_Transactions')),
 ]
