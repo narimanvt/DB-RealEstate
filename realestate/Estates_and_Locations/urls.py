112e6869c9457be27b1b4c30d853666045eaba59
Mahdye-Asadi <mahdye.asaadi@gmail.com>
2024-02-01 20:45:50 +0330

    add part of database to first page and css/html changes

--- realestate/Estates_and_Locations/urls.py
+++ realestate/Estates_and_Locations/urls.py
@@ -5,5 +5,5 @@ app_name = "Estates_and_Locations"
 urlpatterns = [
     path('', views.main_page, name="main_page"),
     path('options/', views.options, name="options"),
-    path('options/details', views.details, name="details"),
+    path('property/', views.property, name="property"),
 ]
