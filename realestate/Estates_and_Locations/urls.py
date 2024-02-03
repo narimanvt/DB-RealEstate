6ed537d8a43ec067cb3004d23bcc4cc70eb814aa
Mahdye-Asadi <mahdye.asaadi@gmail.com>
2024-02-03 11:46:41 +0330

    estate-form(incompelete)

--- realestate/Estates_and_Locations/urls.py
+++ realestate/Estates_and_Locations/urls.py
@@ -4,6 +4,6 @@ from . import views
 app_name = "Estates_and_Locations"
 urlpatterns = [
     path('', views.main_page, name="main_page"),
-    path('options/', views.options, name="options"),
+    path('estate_form/', views.estate_form, name="estate_form"),
     path('property/', views.property, name="property"),
 ]
