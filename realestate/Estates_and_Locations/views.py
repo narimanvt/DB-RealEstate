4f179be73303aaa355fbb8c87b885e0b09a2a6ba
Mahdye-Asadi <mahdye.asaadi@gmail.com>
2024-02-03 22:03:05 +0330

    property file design and filter

--- realestate/Estates_and_Locations/views.py
+++ realestate/Estates_and_Locations/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 from .models import Estate_status, Estate_type, City, Estate
+from Contracts_and_Transactions.models import Contract
 # Create your views here.
 
 def main_page(request):
@@ -19,21 +20,48 @@ def main_page(request):
 
 def property(request):
     filtered_estates = Estate.objects.all()
+    filtering = Contract.objects.all()
     template = loader.get_template("property.html")
 
-    type_id = request.GET.get('type')
+    type = request.GET.get('type')
     status_id = request.GET.get('status')
-    city_id = request.GET.get('city') 
+    city = request.GET.get('city') 
 
-    if type_id:
-        filtered_estates = filtered_estates.filter(estate_type_id=type_id)
+    if type:
+        filtered_estates = filtered_estates.filter(estate_type_id=type)
     elif status_id:
-        filtered_estates = filtered_estates.filter(estate_status_id = status_id)
-    elif city_id:
-        filtered_estates = filtered_estates.filter(city_id = city_id)
+        filtered_estates = filtered_estates.filter(estate_status_id=status_id)
+    elif city:
+        filtered_estates = filtered_estates.filter(city=city)
         
+
+    state = request.GET.get('state')
+    district = request.GET.get('district')
+    no_bedroom = request.GET.get('bedroom')
+    no_bathroom = request.GET.get('bathroom')
+    no_balcony = request.GET.get('balcony')
+    floor_space = request.GET.get('floor')
+    payment = request.GET.get('payment')
+
+    if state:
+        filtered_estates = filtered_estates.filter(estate_status_id=state)
+    elif district:
+        filtered_estates = filtered_estates.filter(district=district)
+    elif no_bedroom:
+        filtered_estates = filtered_estates.filter(number_of_bedrooms=no_bedroom)
+    elif no_bathroom:
+        filtered_estates = filtered_estates.filter(number_of_bathrooms=no_bathroom)
+    elif no_balcony:
+        filtered_estates = filtered_estates.filter(number_of_balconies=no_balcony)
+    elif floor_space:
+        filtered_estates = filtered_estates.filter(floor_space=floor_space)
+    elif payment:
+        filtering = filtering.filter(payment_amount=payment)
+    
+
     context = {
         'filtered_estates' : filtered_estates,
+        'filtering' : filtering,
     }
     return HttpResponse(template.render(context, request))
 
