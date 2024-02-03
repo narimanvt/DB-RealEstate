baee1b43a67070b32a5356e0a1bb8345dc6fd04b
narimanvt <narimanvatankhah81@gmail.com>
2024-02-03 13:22:50 +0330

    .

--- realestate/Clients_and_Contacts/models.py
+++ realestate/Clients_and_Contacts/models.py
@@ -6,7 +6,6 @@ class Client(models.Model):
     last_name = models.CharField(max_length=255, null=False)
     national_code = models.IntegerField(null=False, unique=True, primary_key =True)
     address = models.CharField(max_length=255, null=False)
-    contact_person = models.CharField(max_length=255, null=True)
     phone_number = models.CharField(max_length=64, null=False)
     email = models.EmailField(null=True)
     client_details = models.TextField(null=True)
@@ -21,15 +20,18 @@ class Employee(models.Model):
     last_name = models.CharField(max_length=64, null=True)
     national_code = models.IntegerField(null=False, unique=True)
     phone_number = models.IntegerField(null=False, unique=True)
+    email = models.EmailField(null=True)
+    client_details = models.TextField(null=True)
+    username = models.CharField(max_length=64, null=False)
+    password = models.CharField(max_length =64, null=False)
 
     def __str__(self):
         return f"Employee with id {self.id}"
     
 class Contact(models.Model):
-    client_id =  models.ForeignKey(Client, on_delete=models.CASCADE)
     employee = models.ForeignKey(Employee, on_delete=models.CASCADE, null=False)
     estate = models.ForeignKey('Estates_and_Locations.Estate', on_delete=models.CASCADE, null=False)
-    contact_time = models.TimeField(null=True)
+    contact_time = models.DateTimeField(null=True)
     detail = models.TextField(null=True)
 
     def __str__(self):
